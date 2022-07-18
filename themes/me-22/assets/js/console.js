f413666d40635d3fa48b31d1df44adc4e41e5e86
Stefano Pulze <stefano.pulze@infocamere.it>
2022-07-18 15:24:07 +0200

    Add cookie consent

---
+++ themes/me-22/assets/js/console.js
@@ -0,0 +1,12 @@
+const styles = [
+    'font-size: 24px',
+    'font-family: monospace',
+    'background: white',
+    'display: inline-block',
+    'color: black',
+    'padding: 8px 19px',
+    'border: 1px dashed',
+].join(';')
+
+console.log('%c Hi dude! 👋', styles);
+console.log('%c I\'m available for new jobs', 'font-size: 14px')
